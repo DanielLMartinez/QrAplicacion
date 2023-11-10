8219f55c2bdfc55afd67ebcf9e74ac32d74fdd27
Sergio-Mellado <107446622+Sergio-Mellado@users.noreply.github.com>
2023-11-10 20:07:15 -0300

    creacion y coneccion con base de adatos firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,19 @@ import { IonicModule } from '@ionic/angular';
 import { IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({"projectId":"qrappdani","appId":"1:131965098378:web:78b4f10e258518a6ad9073","databaseURL":"https://qrappdani-default-rtdb.firebaseio.com","storageBucket":"qrappdani.appspot.com","apiKey":"AIzaSyBxBjR9mhHQEpNizl01D5PlhvCewslsYdI","authDomain":"qrappdani.firebaseapp.com","messagingSenderId":"131965098378"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideAppCheck(() => {
+  // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+  const provider = new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
+  return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+}), provideDatabase(() => getDatabase())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
